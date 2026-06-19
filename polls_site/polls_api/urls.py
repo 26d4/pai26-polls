81ff6319bbbb3f9b7874c761d70e41f62e298c54
Witold Węglarz <wrglarz25@gmail.com>
2026-06-19 18:03:22 +0200

    trailing slashes on urls

--- polls_site/polls_api/urls.py
+++ polls_site/polls_api/urls.py
@@ -14,7 +14,7 @@ app_name = 'polls_api'
 urlpatterns = [
 	path('polls/', views.PollListView.as_view(), name='api-poll-list'),
 	path('poll/<int:pk>/', views.PollView.as_view(), name='api-poll'),
-	path('poll/<int:id>/vote', views.poll_vote_cast, name='api-poll-vote-cast'),
+	path('poll/<int:id>/vote/', views.poll_vote_cast, name='api-poll-vote-cast'),
 	path('poll/', views.PollCreateView.as_view(), name='api-poll-create'),
 	path('', include(router.urls)),
 	path('login/', views.LoginView.as_view(), name='api-login'),
--- polls_site/polls_web/urls.py
+++ polls_site/polls_web/urls.py
@@ -5,12 +5,12 @@ import django.contrib.auth.views as auth_views
 app_name = 'polls_web'
 urlpatterns = [
 	path('', views.IndexView.as_view(), name='index'),
-	path('poll/new', views.poll_create, name='new'), # type: ignore
-	path('poll-form-choice-row/<int:number>', views.choice_row),
-	path('poll/<int:pk>', views.DetailView.as_view(), name='detail'),
-	path('poll/<int:pk>/results', views.ResultsView.as_view(), name='results'),
+	path('poll/new/', views.poll_create, name='new'), # type: ignore
+	path('poll-form-choice-row/<int:number>/', views.choice_row),
+	path('poll/<int:pk>/', views.DetailView.as_view(), name='detail'),
+	path('poll/<int:pk>/results/', views.ResultsView.as_view(), name='results'),
 	path('poll/<int:pk>/vote', views.vote_cast, name='vote-cast'), # type: ignore
-	path('login', views.LoginView.as_view(), name='login'),
-	path('logout', auth_views.LogoutView.as_view(), name='logout'),
-	path('register', views.RegisterView.as_view(), name='register')
+	path('login/', views.LoginView.as_view(), name='login'),
+	path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+	path('register/', views.RegisterView.as_view(), name='register')
 ]
